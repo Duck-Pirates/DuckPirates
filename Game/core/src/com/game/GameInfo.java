8071df735cca8e3ff1b232067dbb15f0308cceea
brexit2001 <davidebressani21@gmail.com>
2022-01-18 18:50:13 +0000

    Added loop for ships init and other constants in GameInfo

--- Game/core/src/com/game/GameInfo.java
+++ Game/core/src/com/game/GameInfo.java
@@ -8,7 +8,14 @@ public class GameInfo {
    /* A timer value will also be added */
    public Double volumeMUSIC, volumeSOUNDEFFECTS = 0.50;
    public static String[] colleges_names_array = {"Constantine", "Langwith", "Goodricke", "Anne Lister", "Halifax", "James", "Wentworth", "Alcuin", "Vanbrugh", "Derwent"};
-   /* private Boolean time_limit, soundON; */
 
+   /* private Boolean time_limit, soundON; */
+   private static final int maxNumBoats = 5;
+   private static final int maxNumDucks = 50;
+   private static final float coinsPerDuckPerSecond = 1;
+
+   public int getMaxNumBoats(){
+      return maxNumBoats;
+   }
 
 }
--- Game/core/src/com/game/GameInit.java
+++ Game/core/src/com/game/GameInit.java
@@ -2,6 +2,7 @@ package com.game;
 
 import com.badlogic.gdx.Game;
 import sun.security.jgss.wrapper.GSSNameElement;
+import java.util.Random;
 
 public class GameInit extends Game {
 
@@ -45,11 +46,21 @@ public class GameInit extends Game {
 
     public GameInit(){
 
-        /* Colleges Init */
+        /* Colleges and ships Init */
         for (int i = 0; i < 10; i++){
             College college = new College(GameInfo.colleges_names_array[i]);
             collegesArray[i] = college;
+            Random random = new Random();
+            int numShips;
+            numShips = random.nextInt(gameInfo.getMaxNumBoats()-1)+1;
+            /* for (int i = 0; i < numShips; i++){
+                Add these many ships to the college's ships array
+            }
+             */
         }
+
+
+
 
 
     }
