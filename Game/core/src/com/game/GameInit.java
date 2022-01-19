533edcde0bc5886d27c173e196d5a6aac1c0dd74
Alex Davis <abd516@york.ac.uk>
2022-01-19 14:51:52 +0000

    Eddited my camera implementation so that it uses the gameScreen instead of creating my own camera

--- Game/core/src/com/game/GameInit.java
+++ Game/core/src/com/game/GameInit.java
@@ -1,12 +1,13 @@
 package com.game;
 
+import static com.game.utils.Constants.PPM;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -14,8 +15,6 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 // import sun.security.jgss.wrapper.GSSNameElement;
 // import java.util.Random;
-
-import static com.game.utils.Constants.PPM;
 
 public class GameInit extends Game {
 
@@ -26,40 +25,41 @@ public class GameInit extends Game {
      */
 
     GameScreen gameScreen;
+    GameScreen pauseScreen;
     
     private Box2DDebugRenderer b2dr;
     private World world;
-    private Body player, ship;
-    private OrthographicCamera camera;
+    public Body player;
     
     @Override
     public void create(){
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
         
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, w / 2, h / 2);
+        gameScreen = new GameScreen(w, h);
         
         world = new World(new Vector2(0, 0f), false);
         b2dr = new Box2DDebugRenderer();
         
-        player = createBox(0, 10, 32, 32, false);
-        ship = createBox(10, 10, 64, 32, true);
+        player = createBox(0, 0, 32, 32, false);
+        createBox(32, 32, 64, 32, true);
+        createBox(32, 0, 32, 32, true);
     }
     
     @Override
     public void render() {
-        update(Gdx.graphics.getDeltaTime());
+    	float delta = Gdx.graphics.getDeltaTime();
+        update(delta);
         
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+        Gdx.gl.glClearColor(62 / 255f, 95 / 255f, 201/ 255f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
-        b2dr.render(world, camera.combined.scl(PPM));
+        b2dr.render(world, gameScreen.combinedCameraScaled());
     }
 
     @Override
     public void resize(int width, int height) {
-        camera.setToOrtho(false, width / 2, height / 2);
+        
     }
     
     @Override
@@ -67,41 +67,34 @@ public class GameInit extends Game {
         super.dispose();
         world.dispose();
         b2dr.dispose();
+        pauseScreen.dispose();
     }
     
     public void update(float delta) {
     	world.step(1 / 60f, 6, 2);
     	
     	inputUpdate(delta);
-    	cameraUpdate(delta);
+    	gameScreen.cameraUpdate(delta, player);
     }
+    
     public void inputUpdate(float delta) {
     	int horizontalForce = 0;
     	int verticalForce = 0;
     	
     	if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-    		horizontalForce += 1;
-    	}
-    	if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
     		horizontalForce -= 1;
     	}
-    	if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
-    		verticalForce -= 1;
+    	if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+    		horizontalForce += 1;
     	}
     	if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+    		verticalForce -= 1;
+    	}
+    	if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
     		verticalForce += 1;
     	}
     	
     	player.setLinearVelocity(horizontalForce * 5, verticalForce * 5);
-    }
-    
-    public void cameraUpdate(float delta) {
-    	Vector3 position = camera.position;
-    	position.x = player.getPosition().x * PPM;
-    	position.y = player.getPosition().y * PPM;
-    	camera.position.set(position);
-    	
-    	camera.update();
     }
     
     public Body createBox(int x, int y, int width, int height, boolean isStatic) {
--- Game/core/src/com/game/GameScreen.java
+++ Game/core/src/com/game/GameScreen.java
@@ -1,16 +1,20 @@
 package com.game;
 
+import static com.game.utils.Constants.PPM;
+
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen implements Screen {
 
     //screen
-    private Camera camera;
+    private OrthographicCamera camera;
     private Viewport viewport;
 
     //graphics
@@ -24,18 +28,19 @@ public class GameScreen implements Screen {
     private final int WORLD_WIDTH = 0;
     private final int WORLD_HEIGHT = 0;
 
-    GameScreen(){
+    GameScreen(float width, float height){
         camera = new OrthographicCamera();
+        camera.setToOrtho(false, width / 2, height / 2);
     }
 
     @Override
     public void render(float delta) {
-
+    	
     }
 
     @Override
     public void resize(int width, int height) {
-
+    	camera.setToOrtho(false, width / 2, height / 2);
     }
 
     @Override
@@ -54,12 +59,25 @@ public class GameScreen implements Screen {
     }
 
     @Override
+    public void show() {
+
+    }
+    
+    @Override
     public void dispose() {
 
     }
-
-    @Override
-    public void show() {
-
+    
+    public void cameraUpdate(float delta, Body player) {
+    	Vector3 position = camera.position;
+    	position.x = player.getPosition().x * PPM;
+    	position.y = player.getPosition().y * PPM;
+    	camera.position.set(position);
+    	
+    	camera.update();
+    }
+    
+    public Matrix4 combinedCameraScaled() {
+    	return camera.combined.scl(PPM);
     }
 }
